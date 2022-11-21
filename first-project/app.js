51985ae894c0078b89a18219be57ce1a85269330
Beni <75453780+benib3@users.noreply.github.com>
2022-11-21 22:59:20 +0100

    ejs, for loops and if's statements

--- first-project/app.js
+++ first-project/app.js
@@ -2,22 +2,32 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const app = express();
+const ejs = require("ejs");
+
+//setting template engines
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", function (req, res) {
-  const hmtFilePath = path.join(__dirname, "views", "index.html");
-  res.sendFile(hmtFilePath);
+  res.render("index");
 });
 
 app.get("/restaurants", function (req, res) {
-  const hmtFilePath = path.join(__dirname, "views", "restaurants.html");
-  res.sendFile(hmtFilePath);
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+
+  const fileData = fs.readFileSync(filePath);
+
+  const storedRestaurants = JSON.parse(fileData);
+  res.render("restaurants", {
+    numberOfRestaurants: storedRestaurants.length,
+    restaurants: storedRestaurants,
+  });
 });
 app.get("/recommend", function (req, res) {
-  const hmtFilePath = path.join(__dirname, "views", "recommend.html");
-  res.sendFile(hmtFilePath);
+  res.render("recommend");
 });
 
 app.post("/recommend", function (req, res) {
@@ -25,7 +35,7 @@ app.post("/recommend", function (req, res) {
   const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
-  console.info(JSON.parse(fileData));
+
   const storedRestaurants = JSON.parse(fileData);
 
   storedRestaurants.push(restaurant);
@@ -36,12 +46,10 @@ app.post("/recommend", function (req, res) {
 });
 
 app.get("/confirm", function (req, res) {
-  const hmtFilePath = path.join(__dirname, "views", "confirm.html");
-  res.sendFile(hmtFilePath);
+  res.render("confirm");
 });
 app.get("/about", function (req, res) {
-  const hmtFilePath = path.join(__dirname, "views", "about.html");
-  res.sendFile(hmtFilePath);
+  res.render("about");
 });
 
 app.listen(3000);
