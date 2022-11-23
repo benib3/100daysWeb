b734bd69883975dc97e1be7c3af42f0ec7843f56
Beni <75453780+benib3@users.noreply.github.com>
2022-11-23 18:04:38 +0100

    error pages

--- first-project/app.js
+++ first-project/app.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const app = express();
 const ejs = require("ejs");
+const uUid = require("uuid");
 
 //setting template engines
 app.set("views", path.join(__dirname, "views"));
@@ -26,12 +27,28 @@ app.get("/restaurants", function (req, res) {
     restaurants: storedRestaurants,
   });
 });
+
+app.get("/restaurants/:resId", function (req, res) {
+  const filePath = path.join(__dirname, "data", "restaurants.json");
+  const restaurantId = req.params.resId;
+  const fileData = fs.readFileSync(filePath);
+  const storedRestaurants = JSON.parse(fileData);
+
+  for (const restaurant of storedRestaurants) {
+    if (restaurant.resId === restaurantId) {
+      return res.render("restaurant-details", { restaurant: restaurant });
+    }
+  }
+  res.status(404).render("404");
+});
+
 app.get("/recommend", function (req, res) {
   res.render("recommend");
 });
 
 app.post("/recommend", function (req, res) {
   const restaurant = req.body;
+  restaurant.resId = uUid.v4();
   const filePath = path.join(__dirname, "data", "restaurants.json");
 
   const fileData = fs.readFileSync(filePath);
@@ -50,6 +67,16 @@ app.get("/confirm", function (req, res) {
 });
 app.get("/about", function (req, res) {
   res.render("about");
+});
+
+//invalid inputs for routes
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+
+//error page for server side crash
+app.use(function (error, req, res, next) {
+  res.status(500).render("500");
 });
 
 app.listen(3000);
